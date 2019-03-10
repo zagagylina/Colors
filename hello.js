84ce0fc151a617bdb65cc96ac75db2a0485538e4
Anton Novikov <zagagylina@gmail.com>
2019-03-10 18:11:49 +0200

    new branch - new colors

--- hello.js
+++ hello.js
@@ -1,3 +1,6 @@
 var colors = require('colors');
 
 console.log(colors.yellow('Hello ') + colors.blue('World!'));
+console.log(colors.random('Hello ') + colors.trap('World!'));
+console.log(colors.yellow.bold('Hello ') + colors.blue.inverse('World!'));
+
